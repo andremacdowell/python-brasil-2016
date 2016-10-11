2e501285a425ca07ef0fa1f6ba0c4fcf438eccba
Andre MacDowell <adowell@stone.com.br>
2016-10-11 17:58:03 -0300

    Atualiza authenticate para retornar bad request se nao encontrado header de token

--- samples/falcon/authentication.py
+++ samples/falcon/authentication.py
@@ -3,13 +3,15 @@ import falcon
 
 VALID = 'python-brasil-2016'
 
+
 def _verify_authentication(token):
     return token == VALID
+
 
 def authenticate(request, response, resource, params):
     token = request.get_header('token')
     if not token:
-        raise falcon.HTTPError(falcon.HTTP_401, 'Error',
+        raise falcon.HTTPError(falcon.HTTP_400, 'Error',
                                'Please provide an authentication token.')
     if not _verify_authentication(token):
         raise falcon.HTTPError(falcon.HTTP_401,
