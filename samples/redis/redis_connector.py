debd20589d423b120ad39be98658f96e56b4cf26
Andre MacDowell <adowell@stone.com.br>
2016-10-13 13:35:12 -0300

    Adiciona tratamento para uma custom_exception de Redis nao disponivel

--- samples/redis/redis_connector.py
+++ samples/redis/redis_connector.py
@@ -1,5 +1,7 @@
 # encoding: utf-8
 import redis
+import json
+import custom_exceptions
 from singleton import Singleton
 
 
@@ -8,3 +10,24 @@ class RedisConnector(object):
 
     def __init__(self, config_params):
         self.pool = redis.ConnectionPool(**config_params)
+
+    def get(self, key):
+        sr = self._get_strict_redis()
+        return sr.get(key)
+
+    def set(self, key, value):
+        sr = self._get_strict_redis()
+        return sr.set(key, value)
+
+    def retrieve_whole_db(self, db):
+        sr = self._get_strict_redis()
+        value = map(lambda x: {x: json.loads(sr.get(x))},
+                    sr.keys())
+        return value
+
+    def _get_strict_redis(self):
+        try:
+            sr = redis.StrictRedis(connection_pool=self.pool)
+        except redis.exceptions.ConnectionError as e:
+            raise custom_exceptions.RedisUnavailable(e)
+        return sr
