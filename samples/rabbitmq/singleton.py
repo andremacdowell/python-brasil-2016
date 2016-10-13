78add85eef5fe23ef6b76867188a72f059c4b1e5
Andre MacDowell <adowell@stone.com.br>
2016-10-13 12:38:50 -0300

    Implementa dispatch no Singleton

--- samples/rabbitmq/singleton.py
+++ samples/rabbitmq/singleton.py
@@ -8,5 +8,9 @@ class Singleton(type):
         if cls not in cls._instances:
             cls._instances[cls] = super(
                 Singleton, cls).__call__(*args, **kwargs)
-
+        cls.dispatch = cls.__dispatch
         return cls._instances[cls]
+
+    @classmethod
+    def __dispatch(cls):
+        cls._instances = {}
